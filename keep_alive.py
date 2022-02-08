30e8399135066096f5f7d25640ddf6d87987a8b1
MrDarkShadoow <98183554+MrDarkShadoow@users.noreply.github.com>
2022-02-08 16:14:00 -0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://mrdarkshadoow.github.io"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://darkshadoow159258.github.io"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
